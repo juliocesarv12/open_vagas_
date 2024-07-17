e063e4cbdbfd0237a215ba268251c3113569d3fc
julio <juliocesar@seduc.ro.gov.br>
2024-07-17 12:09:22 -0400

    correcao application.rb e correcao erro devise

--- config/application.rb
+++ config/application.rb
@@ -6,22 +6,23 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+def set_locate_configs_and_timezone
+  config.time_zone = 'Brasilia'
+  config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**/*.{rb,yml}').to_s]
+  config.i18n.available_locales = [:en, 'pt-BR']
+  config.i18n.default_locale = 'pt-BR'
+end
+
 module MotaVagas
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    set_locate_configs_and_timezone
+    config.active_job.queue_adapter = :sidekiq
+
     config.autoload_lib(ignore: %w(assets tasks))
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+
   end
 end
